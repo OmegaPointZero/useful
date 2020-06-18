8c7f5e20914d058e066ebe0057a209daf5abbadd
Joshua Campbell <joshua.erik.campbell@gmail.com>
2020-06-17 20:42:05 -0700

    Added Peek
    Added script for peek

---
+++ peek.sh
@@ -0,0 +1,44 @@
+username=`echo $(whoami)`
+
+# If we don't get an argument, search the current directory
+
+
+
+directory(){
+    stuff=`ls -t $1 | head `
+    for thing in $stuff
+    do
+        typestr=`file $thing`
+        IFS=': ' read -r -a array <<< "$typestr"
+        type=${array[1]}
+        if [ "$type" == "directory" ]; then
+            echo -e "\e[34m$thing [d-$type]"
+        elif [ "$type" == "CSV" ]; then
+            echo -e "\e[36m$thing"
+        elif [ "$type" == "Zip" ]; then
+            echo -e "\e[31m$thing"
+        else
+            echo -e "\e[37m$thing"
+        fi
+    done
+}
+
+if [ -z "$1" ]
+    then
+        directory
+else
+    tgt=$1
+    typ=`file $tgt`
+    IFS=': ' read -r -a array <<< "$typ"
+    type=${array[1]}
+    if [ "$type" == "directory" ]; then
+        directory
+    elif [ "$type" == "CSV" ]; then
+        stuff=`cat $tgt | head`
+        IFS=' ' read -r -a array <<< "$stuff"
+        for thing in $stuff
+        do
+            echo "$thing"
+        done
+    fi
+fi
